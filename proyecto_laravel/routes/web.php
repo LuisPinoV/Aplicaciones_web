ab3ea804daf41fc530943bd9fe33be27a895e028
LuisPinoV <144037119+LuisPinoV@users.noreply.github.com>
2025-11-03 16:38:36 -0300

    Revert "dashboard laravel"
    This reverts commit ed88bd28f3f6332bae6ab138ce4e74d0fd434c2a.

--- proyecto_laravel/routes/web.php
+++ proyecto_laravel/routes/web.php
@@ -1,24 +1,15 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+use App\Http\Controllers\FichasController;
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+Route::get('/fichas', [FichasController::class, 'index']);
 
 use App\Http\Controllers\DashboardController;
 
-Route::get('/mi_dashboard', [DashboardController::class, 'mi_dashboard']);
+Route::get('/dashboard', [DashboardController::class, 'dashboard']);
